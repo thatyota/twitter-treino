26fa41c18cafd6059a632d15c9cb9fa2e9298383
thatyota <kunarata32@gmail.com>
2026-02-01 13:32:05 +0000

    StartServer function added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
-export const startserver = async () => {
+export const connectdb = async () => {
   try{
    await mongoose.connect(ENV.MONGO_URI)
    console.log("Connected to DB SUCCESSFULLY")
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 
-import {  startserver } from "./config/db.js";
+import {  connectdb } from "./config/db.js";
 
 const app = express();
 
 
-startserver();
 
 app.get("/",(req,res) => res.send("Hello from server "))
 
+const startserver = async () => {
+  try{
+   await connectdb();
+    
+   app.listen(ENV.PORT, () => console.log("Server is app and running on PORT:",ENV.PORT));
 
-app.listen(ENV.PORT, () => console.log("Server is app and running on PORT:",ENV.PORT));
+  } catch (error) {
+     console.error("Failed to connect to server", error.mensage);
+     process.exit(1);
+  }
+  
+   
+};
+
+startserver();
