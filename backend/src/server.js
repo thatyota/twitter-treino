5e3d98508fafdfebbc96bfbf039d5b8523727c07
thatyota <kunarata32@gmail.com>
2026-02-03 10:30:58 +0000

    hell yeah

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { ENV } from "./config/env.js";
-
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
 import {  connectdb } from "./config/db.js";
 
 const app = express();
 
-
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware());
 
 app.get("/",(req,res) => res.send("Hello from server "))
+
+app.use("/api/users",userRoutes)
 
 const startserver = async () => {
   try{
